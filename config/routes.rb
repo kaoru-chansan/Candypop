f785da9a3327c3c1054939c19baa311339d0cb11
EC2 Default User <151661528+kaoru-chansan@users.noreply.github.com>
2024-04-12 14:56:33 +0000

    viwe + controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,24 @@
 Rails.application.routes.draw do
+  namespace :public do
+    get 'relationships/followings'
+    get 'relationships/followers'
+  end
+  namespace :public do
+    get 'posts/show'
+    get 'posts/edit'
+    get 'posts/new'
+    get 'posts/index'
+  end
+  namespace :public do
+    get 'customers/show'
+    get 'customers/edit'
+    get 'customers/unsubscribe'
+  end
+  namespace :admin do
+    get 'customers/index'
+    get 'customers/show'
+    get 'customers/edit'
+  end
   devise_for :admins
   devise_for :customers
   get 'post_lists/index'
