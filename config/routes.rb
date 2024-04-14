5970372a5ecbe32e50ad15c78836920461db9481
EC2 Default User <151661528+kaoru-chansan@users.noreply.github.com>
2024-04-14 06:48:21 +0000

    controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,39 @@
 Rails.application.routes.draw do
-  namespace :public do
-    get 'relationships/followings'
-    get 'relationships/followers'
-  end
-  namespace :public do
-    get 'posts/show'
-    get 'posts/edit'
-    get 'posts/new'
-    get 'posts/index'
-  end
-  namespace :public do
-    get 'customers/show'
-    get 'customers/edit'
-    get 'customers/unsubscribe'
-  end
+
+  devise_for :customers, skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+  }
+
+  devise_for :admin, controllers: {
+  sessions: "admin/sessions"
+  }
+
+
   namespace :admin do
     get 'customers/index'
     get 'customers/show'
     get 'customers/edit'
   end
-  devise_for :admins
-  devise_for :customers
+
+scope module: :public do
+  root to: 'homes#top'
   get 'post_lists/index'
   get 'post_lists/show'
   get 'homes/top'
   get 'homes/admout'
+
+  get 'customers/show'
+  get 'customers/edit'
+  get 'customers/unsubscribe'
+
+  get 'posts/show'
+  get 'posts/edit'
+  get 'posts/new'
+  get 'posts/index'
+
+  get 'relationships/followings'
+  get 'relationships/followers'
+end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
