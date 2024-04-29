7494761fbface6d320587ea8f953e062e102c627
EC2 Default User <151661528+kaoru-chansan@users.noreply.github.com>
2024-04-29 15:07:10 +0000

    follow 1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_04_11_153157) do
+ActiveRecord::Schema.define(version: 2024_04_29_141853) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -92,6 +92,13 @@ ActiveRecord::Schema.define(version: 2024_04_11_153157) do
   create_table "posts", force: :cascade do |t|
     t.integer "customer_id", null: false
     t.text "caption", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
